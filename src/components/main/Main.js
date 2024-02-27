16cefeb34020aa3908d2f73d7ebdb521475bf989
d-pozniak <pozniakdi@gmail.com>
2024-02-27 03:24:51 +0300

    feat: + sections into main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,15 @@
 import HeroSection from "../heroSection/HeroSection";
+import Specials from "../specials/Specials";
+import CustomersSay from "../customersSay/CustomersSay";
+import Chicago from "../chicago/Chicago";
 
 export default function Main() {
     return (
         <main>
             <HeroSection/>
+            <Specials/>
+            <CustomersSay/>
+            <Chicago/>
         </main>
     )
 }
