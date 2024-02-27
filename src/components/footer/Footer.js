03e3c39c37b658ccffc9e38493cd69cbc453e8c4
d-pozniak <pozniakdi@gmail.com>
2024-02-27 03:30:31 +0300

    feat: + footer content, styles

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,20 @@
+import center from '../../helpers/center.module.css';
+import Lemon from "../../assets/images/lemon.png";
+import styles from './Footer.module.css'
+
 export default function Footer() {
     return (
-        <footer></footer>
+        <footer className={`${styles.footer} ${center.max__width}`}>
+            <img
+                src={Lemon}
+                className={styles.logo}
+                alt='Little Lemon'
+            />
+            <div>
+                <h4 className={styles.contact}>Contact Us</h4>
+                <div>Chicago, Illinois</div>
+                <div>(123)-456-7891</div>
+            </div>
+        </footer>
     )
 }
